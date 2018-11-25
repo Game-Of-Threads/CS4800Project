5a7ecb190a888238045a34e47e176a99369e9cd4
Julio Berina <julioberina@gmail.com>
2018-11-25 01:19:18 -0800

    Got Google OAuth working

--- src/LoginComponent.jsx
+++ src/LoginComponent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import GoogleLogin from 'react-google-login';
 import AppContext from './AppProvider.jsx'
+import { Redirect } from 'react-router-dom';
 
 class LoginComponent extends Component {
     constructor(props) {
@@ -10,9 +11,31 @@ class LoginComponent extends Component {
             message: "", // Multi-purpose message to be displayed to the user, blank by default
             messageStatus: "black", //color of the message
             // NOTE: "success" for green "warning" for yellow "danger" for red
+            // Redirect if logged in
+            redirect: false
         }
+
         this.updateState = this.updateState.bind(this);
         this.onSignInFailure = this.onSignInFailure.bind(this);
+        this.signInUser = function(that) {
+          return function(response) {
+            let userData;
+
+            if (response.w3.U3) {
+              userData = {
+                name: response.w3.ig,
+                provider: 'google',
+                email: response.w3.U3,
+                provider_id: response.El,
+                token: response.Zi.access_token,
+                provider_pic: response.w3.Paa
+              }
+
+              sessionStorage.setItem("userData", userData);
+              that.setState({redirect: true});
+            }
+          };
+        }
     }
     updateState(e) {
         //Updates the state of the component on every change.
@@ -23,10 +46,15 @@ class LoginComponent extends Component {
         })
     }
 
-    onSignInFailure(){
+    onSignInFailure(response) {
       console.log("Sign In failed");
     }
+
     render() {
+        if (this.state.redirect || sessionStorage.getItem('userData')) {
+          return (<Redirect to={'/notebook'} />);
+        }
+
         return (
           <AppContext.Consumer>
             {(context) => (
@@ -37,7 +65,7 @@ class LoginComponent extends Component {
                   <GoogleLogin
                     clientId="690986198979-e6btiprgsgp69hlrc5p589bsnnfbikue.apps.googleusercontent.com"
                     buttonText="Log In with Google"
-                    onSuccess={context.signInUser}
+                    onSuccess={this.signInUser(this)}
                     onFailure={this.onSignInFailure}/>
                   <br></br>
                 </div>
